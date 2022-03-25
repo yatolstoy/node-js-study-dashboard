772add15449ea0761a81e7745a76ce3bf16bfe77
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-03-25 17:42:34 +0300

    add: JWT token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,12 +11,15 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { IUserService } from './user.service.interface';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.Logger) private loggerService: ILogger,
 		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		4;
@@ -43,11 +46,12 @@ export class UserController extends BaseController implements IUserController {
 		next: NextFunction,
 	): Promise<void> {
 		const userValidate = await this.userService.validate(body);
-		if (userValidate) {
-			this.ok(res, { jwt: 'someJWT' });
+		if (!userValidate) {
+			next(new HTTPError('Ошибка авторизации', 401, 'login'));
 			return;
 		}
-		next(new HTTPError('Ошибка авторизации', 401, 'login'));
+		const jwt = await this.signJWTToken(body.login);
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -58,5 +62,10 @@ export class UserController extends BaseController implements IUserController {
 		const result = await this.userService.create(body);
 		if (!result) return next(new HTTPError('Такой пользователь уже существует', 433, 'Register'));
 		this.ok(res, { login: result.login, id: result.id });
+	}
+
+	async signJWTToken(login: string): Promise<string> {
+		const key = this.configService.get('JWT-KEY');
+		return await sign({ login, iat: Math.ceil(Date.now() / 1000) }, key, { algorithm: 'HS256' });
 	}
 }
