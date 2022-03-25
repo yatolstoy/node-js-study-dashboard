44a841fecd278c830e2d658681b3e3a296894aa3
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-03-25 23:15:02 +0300

    refactor: create promise for asyncVerify function

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -1,22 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
-import { injectable } from 'inversify';
-import { verify } from 'jsonwebtoken';
+import { JwtPayload, verify } from 'jsonwebtoken';
 import { IMiddleware } from './middleware.interface';
 
 export class AuthMiddleware implements IMiddleware {
 	constructor(private secret: string) {}
-	execute(req: Request, res: Response, next: NextFunction): void {
+	async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
 		const header = req.headers.authorization;
 		if (header && header.indexOf('Bearer ') !== -1) {
 			const jwt = header.split(' ')[1];
-			const isVerify = verify(jwt, this.secret, (err, payload) => {
-				if (err) {
-					next();
-				} else if (payload && typeof payload !== 'string') {
-					req.user = payload.login;
-				}
-			});
+			const payload = await this.asyncVerify(jwt);
+			req.user = payload.login;
 		}
 		next();
+	}
+
+	async asyncVerify(jwt: string): Promise<JwtPayload> {
+		return new Promise((resolve, reject) => {
+			verify(jwt, this.secret, (err, payload) => {
+				if (!err && payload && typeof payload !== 'string') {
+					resolve(payload);
+				} else {
+					reject();
+				}
+			});
+		});
 	}
 }
